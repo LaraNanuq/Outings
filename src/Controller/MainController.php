6d70c3bac99b1c95ce7a9c0e6f9215dfdc365887
Ayouli <ayelen.dumas@gmail.com>
2021-07-12 20:14:48 +0200

    Add OutingController, MainControllers and the twigs files

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->redirectToRoute('outing_list');
+    }
+}
--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -9,12 +9,52 @@ use Symfony\Component\Routing\Annotation\Route;
 class OutingController extends AbstractController
 {
     /**
-     * @Route("/outing", name="outing")
+     * @Route("/outing/list", name="outing_list")
      */
-    public function index(): Response
+    public function list(): Response
     {
-        return $this->render('outing/index.html.twig', [
+        return $this->render('outing/list.html.twig', [
             
+        ]);
+    }
+
+    /**
+     * @Route("/outing/create", name="outing_create")
+     */
+    public function create(): Response
+    {
+        return $this->render('outing/create.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/outing/detail/{id}", name="outing_detail")
+     */
+    public function detail(): Response
+    {
+        return $this->render('outing/detail.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/outing/edit/{id}", name="outing_edit")
+     */
+    public function edit(): Response
+    {
+        return $this->render('outing/edit.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/outing/cancel/{id}", name="outing_cancel")
+     */
+    public function cancel(): Response
+    {
+        return $this->render('outing/cancel.html.twig', [
+
         ]);
     }
 }
